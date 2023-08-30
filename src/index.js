b5a2bc88340ceb8c4d90274ce88ab995a7d4e33f
ABHIJEET <abhijeetsahoomsd@gmail.com>
2023-08-30 08:57:08 +0530

    Exported tweet repository and added tweet to our database

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ const express = require('express');
 const connect = require('./config/database');
 
 const app = express();
+const TweetRepository = require('./repository/tweet-repository');
+
 
 app.listen(3000, async () => {
     console.log(`Server started`);
     await connect();
     console.log('MongoDB connected');
+    const tweet = await Tweet.create({
+        content: 'First tweet',
+        userEmail: 'a@b .com'
+    });
+    console.log(tweet);
 });
