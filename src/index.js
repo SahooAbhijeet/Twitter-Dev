903bb0e8493b00cf664f00f544841c1c4e3647d4
ABHIJEET <abhijeetsahoomsd@gmail.com>
2023-09-17 16:41:11 +0530

    added routes and controller for tweet api

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import { connect } from './config/database.js';
-const app = express();
+import apiRoutes from "./routes/index.js";
 
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/api', apiRoutes);
 
 import TweetService from './services/tweet-service.js';
 
