24a7f3c663bbaf523cd476a42b0d67df12e5398d
ABHIJEET <abhijeetsahoomsd@gmail.com>
2023-12-24 15:20:20 +0530

    added whole logic to server

---
+++ server/src/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { connect } from './config/database.js';
+import apiRoutes from "./routes/index.js";
+import passport from 'passport';
+
+import { passportAuth } from "./config/jwt-middleware.js";
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(passport.initialize());
+passportAuth(passport);
+
+app.use('/api', apiRoutes);
+
+
+app.listen(3000, async () => {
+    console.log(`Server started`);
+    await connect();
+    console.log('MongoDB connected');
+});     
